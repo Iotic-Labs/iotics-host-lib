d104eec89e8a2c28d4be146f98ecd25c180a86d7
Adrian <adrian.dzula@iotic-labs.com>
2021-03-04 18:42:00 +0000

    WIP EG-86 ioticsctl - part 2

--- scripts/gen_creds.py
+++ scripts/gen_creds.py
@@ -22,40 +22,40 @@ SEED_CACHE_FILE = Path('user_seed.txt')
 
 
 def main():
-    if os.getenv('RESOLVER'):
-        print(f'A simple helper script to generate DIDs and seeds using `{RESOLVER}` resolver.')
-    else:
-        print(f'`RESOLVER` environment variable not found, defaulting to `{RESOLVER}`.')
-        os.environ['RESOLVER'] = RESOLVER
+    check_resolver()
     if not SEED_CACHE_FILE.is_file():
         answer = input('Creating a new user seed, continue? [y]: ')
         if answer != 'y':
             return
 
-    project_seed, created = get_or_create_project_seed()
+    user_seed, created = get_or_create_user_seed()
     if created:
-        store_seed(project_seed)
+        store_seed(user_seed)
     else:
         print(f'Using an existing user seed from `{SEED_CACHE_FILE.absolute()}`.')
 
-    user_did, _ = get_or_create_user_identity(project_seed)
-    name = input('Creating a seed for an agent, provide a name for your agent (e.g.: `agent-follower` or leave empty to abort): ')
-    if not name:
+    user_doc, _ = get_or_create_user_identity(user_seed)
+    answer = input('Creating a new agent seed, continue? [y]: ')
+    if answer != 'y':
         return
 
-    agent_seed = create_agent(project_seed, name)
-    if not agent_seed:
-        print('An agent for the given user seed and name already exists.')
-        return
-
+    agent_seed = create_agent(user_doc)
     print('A new agent has been created, use the following variables for your connector:\n')
     print(f'export RESOLVER_HOST={RESOLVER}')
-    print(f'export HOST_USER={user_did}')
+    print(f'export HOST_USER={user_doc.id}')
     print(f'export SEED={agent_seed}')
     print('\nRemember to store the above values as you may loose control of your connectors otherwise.')
 
 
-def get_or_create_project_seed():
+def check_resolver():
+    if os.getenv('RESOLVER'):
+        print(f'A simple helper script to generate DIDs and seeds using `{RESOLVER}` resolver.')
+    else:
+        print(f'`RESOLVER` environment variable not found, defaulting to `{RESOLVER}`.')
+        os.environ['RESOLVER'] = RESOLVER
+
+
+def get_or_create_user_seed():
     """Creates a random or finds an existing seed for a project (used to create user/agent DIDs)."""
     created = False
     if SEED_CACHE_FILE.is_file():
@@ -77,44 +77,41 @@ def store_seed(seed):
         print('It is NOT advised to store a seed in a file on a production environment.')
 
 
-def get_or_create_user_identity(project_seed):
+def get_or_create_user_identity(user_seed):
     """Creates a new or finds an existing user identity (DID)."""
-    did, created = _get_doc(Identifier.DIDType.USER, project_seed)
-    return did, created
+    user_doc, created, _ = _get_doc(Identifier.DIDType.USER, user_seed)
+    return user_doc, created
 
 
-def create_agent(project_seed, name):
+def create_agent(user_doc):
     """Returns a seed and creates an agent (DID) if agent with given name does not exist."""
-    if name and not name.startswith('#'):
-        name = '#' + name
-    did, created = _get_doc(Identifier.DIDType.AGENT, project_seed, name)
-    if not created:
-        return
-
     agent_seed = Identifier.new_seed(NEW_SEED_LEN)
+    agent_doc, _, private_key = _get_doc(Identifier.DIDType.AGENT, agent_seed)
+
+    proof = Document.new_proof(user_doc.id.encode('ascii'), private_key)
+    delegation = Document.new_delegation('#agent', agent_doc.id + agent_doc.public_keys[0].id, proof)
+    user_doc.add_authentication_delegation(delegation)
+
     return agent_seed
 
 
-def _get_doc(did_type, project_seed, name=None):
+def _get_doc(did_type, seed_str):
     """Creates a new or finds an existing user/agent identity."""
-    seed = Identifier.seed_to_master(project_seed)
-    if not name:
-        private_key_hex = Identifier.new_private_hex_from_path(seed, did_type, count=0)
-    else:
-        private_key_hex = Identifier.new_private_hex_from_path_str(seed, did_type, name)
+    seed = Identifier.seed_to_master(seed_str)
+    private_key_hex = Identifier.new_private_hex_from_path(seed, did_type, count=0)
     private_key_ecdsa = Identifier.private_hex_to_ECDSA(private_key_hex)
-    doc = Document.new_did_document(did_type, private_key_ecdsa, name or '')
+    doc = Document.new_did_document(did_type, private_key_ecdsa)
     did = doc.id
     try:
         doc = Resolver.discover(did)
     except IdentityNotFound:
-        tkn = Document.new_document_token(doc, RESOLVER, did + doc.public_keys[0].id, private_key_ecdsa)
-        Resolver.register(tkn)
+        token = Document.new_document_token(doc, RESOLVER, did + doc.public_keys[0].id, private_key_ecdsa)
+        Resolver.register(token)
         created = True
     else:
         created = False
 
-    return doc.id, created
+    return doc, created, private_key_ecdsa
 
 
 if __name__ == '__main__':
