fd5fb2f1ec6fc61a69f7e511c3b6422176918067
Lorenzo Paris <paris.lorenzo90@gmail.com>
2021-03-08 17:24:31 +0000

    EG-123: iotics-host-lib - add remote get last feed value
    * Changed InterestApi class to have get_feed_last_stored_local and get_feed_last_stored methods;
    * Added tests for the above

--- src/iotics/host/api/interest_api.py
+++ src/iotics/host/api/interest_api.py
@@ -14,7 +14,7 @@ class InterestApi:
 
     @check_and_retry_with_new_token
     @fill_refs
-    def get_feed_last_stored(
+    def get_feed_last_stored_local(
             self, follower_twin_id: str, followed_twin_id: str, feed_id: str,
             client_ref: str = None, transaction_ref: str = None
     ) -> FetchInterestResponsePayload:
@@ -38,6 +38,42 @@ class InterestApi:
 
         """
         return self.rest_api_client.fetch_last_stored_local(
+            follower_twin_id=follower_twin_id,
+            followed_twin_id=followed_twin_id,
+            followed_feed_id=feed_id,
+            iotics_client_app_id=self.client_app_id,
+            iotics_client_ref=client_ref,
+            iotics_transaction_ref=transaction_ref
+        )
+
+    @check_and_retry_with_new_token
+    @fill_refs
+    def get_feed_last_stored(
+            self, host_id: str, follower_twin_id: str, followed_twin_id: str, feed_id: str,
+            client_ref: str = None, transaction_ref: str = None
+    ) -> FetchInterestResponsePayload:
+        """ Get the latest stored data from a remote digital twin feed
+
+        Args:
+            host_id (str): The ID of the host
+            follower_twin_id (str): The ID of the twin asking for the data
+            followed_twin_id (str): The ID of the twin whose feed has the data
+            feed_id (str): The ID of the feed from which data is sought
+            client_ref (str, optional): to be deprecated, must be unique for each request
+            transaction_ref (str, optional): Used to loosely link requests/responses in a distributed env't. Max 36 char
+
+        Returns: FetchInterestResponsePayload, with the following structure:
+            followed_feed (InterestFollowedFeed):
+                feed (Feed):
+                    id (FeedID), with a single attribute `value`, containing the followed feed's id as a string
+                    twin_id (TwinID), with a single attribute `value`, containing the followed twin's id as a string
+                host_id (HostID): has attribute `value` containing a string identifying the host
+            follower_twin_id (TwinID): has attribute `value` containing the followER twin's id as a string (same as
+                followed twin as currently implemented)
+
+        """
+        return self.rest_api_client.fetch_last_stored(
+            host_id=host_id,
             follower_twin_id=follower_twin_id,
             followed_twin_id=followed_twin_id,
             followed_feed_id=feed_id,
