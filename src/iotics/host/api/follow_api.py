7e32f3c35aabc26a2b7e35c75663cbf072263043
Adrian <adrian.dzula@iotics.com>
2021-04-14 08:10:14 +0100

    EG-194 Update follow and search APIs to reconnect after a host restart
    * Add retry functionality to `_connect` methods that will be triggered after:
      1 second, 10 seconds, 1m40s, 16m40s and then indefinitely every 1h.

--- src/iotics/host/api/follow_api.py
+++ src/iotics/host/api/follow_api.py
@@ -103,6 +103,7 @@ class FollowAPI:
         except Exception as ex:
             raise DataSourcesStompNotConnected(ex) from ex
 
+    @retry(exceptions=ConnectionError, delay=1, backoff=10, max_delay=3600)
     def _connect(self, reconnect_attempts_max: int, heartbeats: Tuple[int, int]):
         """this method also used to handle reconnecting to stomp server if e.g. network connection goes down
         """
