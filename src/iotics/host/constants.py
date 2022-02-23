0bf1b2fc4a89aec5ff018b1e93adfb370608539f
Adrian <adrian.dzula@iotics.com>
2022-02-23 15:51:32 +0000

    EI-781 Add copyright and license information

--- src/iotics/host/constants.py
+++ src/iotics/host/constants.py
@@ -1,3 +1,16 @@
+# Copyright 2022 Iotic Labs Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/Iotic-Labs/iotics-host-lib/blob/master/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 TIMESTEP_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 RFC1123_FORMAT = '%a, %d %b %Y %H:%M:%S %Z'
 IOTICS_DATA_URI = 'http://data.iotics.com/'
