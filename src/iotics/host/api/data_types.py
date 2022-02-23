0c04b9f57d0056a016b2a7eb58498e6cb8005b65
Adrian <adrian.dzula@iotics.com>
2022-02-23 15:51:32 +0000

    EI-781 Add copyright and license information

--- src/iotics/host/api/data_types.py
+++ src/iotics/host/api/data_types.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2020 Iotic Labs Ltd. All rights reserved.
+# Copyright © 2021 to 2022 IOTIC LABS LTD. info@iotics.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://github.com/Iotic-Labs/py-IoticAgent/blob/master/LICENSE
+#     https://github.com/Iotic-Labs/iotics-host-lib/blob/master/LICENSE
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
